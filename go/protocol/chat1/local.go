f4dff3afa55a72fdba177b7f9ef9d9b0035387f1
Jack O'Connor <oconnor663@gmail.com>
2018-06-14 11:50:27 -0400

    use a dummy signing key when pairwise MAC'ing, bumping version to V4

--- go/protocol/chat1/local.go
+++ go/protocol/chat1/local.go
@@ -2329,6 +2329,7 @@ const (
 	MessageUnboxedErrorType_BADVERSION          MessageUnboxedErrorType = 2
 	MessageUnboxedErrorType_IDENTIFY            MessageUnboxedErrorType = 3
 	MessageUnboxedErrorType_EPHEMERAL           MessageUnboxedErrorType = 4
+	MessageUnboxedErrorType_PAIRWISE_MISSING    MessageUnboxedErrorType = 5
 )
 
 func (o MessageUnboxedErrorType) DeepCopy() MessageUnboxedErrorType { return o }
@@ -2339,6 +2340,7 @@ var MessageUnboxedErrorTypeMap = map[string]MessageUnboxedErrorType{
 	"BADVERSION":          2,
 	"IDENTIFY":            3,
 	"EPHEMERAL":           4,
+	"PAIRWISE_MISSING":    5,
 }
 
 var MessageUnboxedErrorTypeRevMap = map[MessageUnboxedErrorType]string{
@@ -2347,6 +2349,7 @@ var MessageUnboxedErrorTypeRevMap = map[MessageUnboxedErrorType]string{
 	2: "BADVERSION",
 	3: "IDENTIFY",
 	4: "EPHEMERAL",
+	5: "PAIRWISE_MISSING",
 }
 
 func (e MessageUnboxedErrorType) String() string {
